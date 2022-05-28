1147ba229441e99da20c9cbb2dece7d0660fab55
Mike <ampgzo2@gmail.com>
2022-05-28 15:15:59 -0400

    left and right nav bar with hamburger menu

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,8 +2,8 @@ import './intro.scss'
 
 export default function Intro() {
   return (
-    <div className="intro">
-      qwdqw
+    <div className="intro" id="intro">
+      Hi my name is Michael Diamond. Welcome to my portfolio page!
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import './navbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 
 export default function Navbar() {
   return (
     <div className="navbar">
-
+      <div className="wrapper">
+        {/* left container start */}
+        <div className="left">
+        <a href="#intro" className = "logo">Intro</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span> 954-295-9085</span>
+          </div>
+          <div className="itemContainer">
+          <Mail className="icon"/>
+          <span> ampgzo2@gmail.com</span>
+          </div>
+        </div>
+        {/* left container end */}
+        {/* right container start */}
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+        {/* right container end */}
+      </div>
     </div>
   )
 }
