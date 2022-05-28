ae5e267f9c7e57c215608af2ca4112932e56df1d
Mike <ampgzo2@gmail.com>
2022-05-28 17:43:20 -0400

    intro component update using ityped, replacing with typewriter-effect

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,42 @@
 import './intro.scss'
+import {KeyboardArrowDown} from '@material-ui/icons'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer"],
+    });
+  },[]);
+
   return (
     <div className="intro" id="intro">
-      Hi my name is Michael Diamond. Welcome to my portfolio page!
+      {/* left side start */}
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/self.jpg" alt=""/>
+        </div>
+      </div>
+      {/* left side end */}
+      {/* right side start */}
+      <div className="right">
+        <div className="wrapper">
+          <h1>Michael Diamond</h1>
+          <h3>
+            Full Stack Web <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDown className="icon"/>
+        </a>
+      </div>
+      {/* right side end */}
     </div>
   )
 }
