989de33245db2a752a1c0a1259a35dc60bf51c64
Mike <ampgzo2@gmail.com>
2022-05-29 19:10:45 -0400

    update push for remote

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.scss'
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/shop-shop.jpg" alt=""/>
+          <h3>Shopping App</h3>
+        </div>
+      </div>
     </div>
   )
 }
